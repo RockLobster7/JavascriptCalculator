46f6f692f6d5f1fc3920d743b18cee9af5ff9e64
Mario Cueva <mario.cueva.au@gmail.com>
2017-10-17 21:16:26 +1100

    handling numbers, zero, and period

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-$(document).ready(function() {
-    
-    
-    
+$(document).ready(function () {
+
+
+    //handle numbers
+    $('.number').click(function () {
+        //if the display is showing 0, then overwrite it with a new number, else append the new number
+        if ($('#display').text() === '0') {
+            $('#display').text($(this).text());
+        } else {
+            $('#display').text($('#display').text() + $(this).text());
+        }
+    });
+
+    //handle period
+    $('#btn-period').click(function () {
+        //append only one period on the display
+        if ($('#display').text().indexOf('.') === -1)
+            $('#display').text($('#display').text() + $(this).text());
+    });
+
+    //handle clear
+    $('#btn-clear').click(() => $('#display').text('0'));
+
+
+
 });
