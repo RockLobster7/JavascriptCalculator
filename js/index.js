54ffee36b84dd71f6bb73a98e2bf2353f15ea027
Mario Cueva <mario.cueva.au@gmail.com>
2017-10-21 21:16:25 +1100

    use 'eval' to evaluate the string expression. Need to replace HTML symbols with javascript operators first

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+   
     //handle numbers
     $('.number').click(function () {
         //if the display is showing 0, then overwrite it with a number, else append
@@ -17,19 +17,22 @@ $(document).ready(function () {
 
     //handle operators
     $('.operator').click(function () {
-        //if the display is showing 0, then overwrite it with a number, else append the operator
+        //if the display is not showing 0, then append the operator
         if ($('#main-display').text() !== '0' && $('#main-display').text() !== '0.') {
 
-            //if the last character in the working display is a period, then remove it and append operator
+            //if the last character in the working display is a period, then remove it to append operator
             let workingDisplay = $('#working-display').text();
             if (workingDisplay[workingDisplay.length - 1] === '.') {
-                $('#working-display').text(workingDisplay.substring(0,workingDisplay.length-1) + $(this).text());
+                $('#working-display').text(workingDisplay.substring(0, workingDisplay.length - 1) + $(this).text());
             } else {
                 $('#working-display').text($('#working-display').text() + $(this).text());
             }
-            
+
+            // alert(runningTotal($('#main-display').text(), $(this).text()));
+
             //zero out the main display for the next number
             $('#main-display').text('0');
+
         };
     });
 
@@ -54,8 +57,16 @@ $(document).ready(function () {
 
     //handle equals
     $('#btn-equals').click(function () {
-        if (/^[^=]+\d$/.test($('#working-display').text()))
+        if (/^[^=]+\d$/.test($('#working-display').text())) {
+
+            let x = $('#working-display').text();
+
             $('#working-display').text($('#working-display').text() + $(this).text());
+
+            //get this working eval will evaluate the string as a mathematical equation but you need to
+            //replace the HTML symbols with operators .e.g. x with * 
+            alert(eval(x));
+        }
     });
 
 
