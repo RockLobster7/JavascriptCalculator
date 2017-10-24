237698dc3c328851b28007aba7bf0a0505f89952
Mario Cueva <mario.cueva.au@gmail.com>
2017-10-24 21:08:57 +1100

    arithmetic working. all controls behaviour working. Need to manage display overflow

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 $(document).ready(function () {
+    //variable controls what should happen after the equals button was pressed
+    var wasEquals = false;
 
     //handle numbers
     $('.number').click(function () {
-        //if the display is showing 0, then overwrite it with a number, else append
+        //if the working display is showing 0, then overwrite it with a new digit
         if ($('#working-display').text() === '0') {
             $('#main-display').text($(this).text());
             $('#working-display').text($(this).text());
+
+            //if the main display is showing 0, then append the digit to the working display
         } else if ($('#main-display').text() === '0') {
             $('#main-display').text($(this).text());
             $('#working-display').text($('#working-display').text() + $(this).text());
+
+            //if wasEquals then overwrite both displays with the new digit
+        } else if (wasEquals) {
+            $('#main-display').text($(this).text());
+            $('#working-display').text($(this).text());
+            wasEquals = false;
+
+            //otherwise append the digit to both displays
         } else {
             $('#main-display').text($('#main-display').text() + $(this).text());
             $('#working-display').text($('#working-display').text() + $(this).text());
@@ -17,14 +29,13 @@ $(document).ready(function () {
 
     //handle operators
     $('.operator').click(function () {
- 
+
         let workingDisplay = $('#working-display').text();
 
         //if the display is not showing 0, then append the operator
         if ($('#main-display').text() !== '0' && $('#main-display').text() !== '0.') {
 
             //if the last character in the working display is a period, then remove it to append operator
- 
             if (workingDisplay[workingDisplay.length - 1] === '.') {
                 $('#working-display').text(workingDisplay.substring(0, workingDisplay.length - 1) + $(this).text());
 
@@ -33,7 +44,6 @@ $(document).ready(function () {
             }
             //zero out the main display for the next number
             $('#main-display').text('0');
-
         };
 
         //swap the operator around if the user changes his mind
@@ -42,8 +52,10 @@ $(document).ready(function () {
         }
     });
 
+
     //handle period
     $('#btn-period').click(function () {
+
         //append only one period per number sequence
         if ($('#main-display').text().indexOf('.') === -1) {
             if ($('#main-display').text() === '0' && $('#working-display').text() !== '0') {
@@ -53,18 +65,22 @@ $(document).ready(function () {
             }
             $('#main-display').text($('#main-display').text() + '.');
         }
+
+        //reset wasEquals so we can append a number instead of overwriting it
+        wasEquals = false;
     });
 
     //handle clear
     $('#btn-clear').click(() => {
         $('#working-display').text('0');
         $('#main-display').text('0');
+        wasEquals = false;
     });
 
     //handle equals
     $('#btn-equals').click(function () {
         //if the string does not already contain an equals and ends with a digit
-        if (/^[^=]+\d$/.test($('#working-display').text())) {
+        if (/\d+(\.?$)(\d$)?/.test($('#working-display').text())) {
 
             // convertHTML symbols to javascript arithmetic operators
             var html = {
@@ -76,7 +92,7 @@ $(document).ready(function () {
             let expr = $('#working-display').text()
                 .replace(/[÷×−+]/g, (char) => html[char]);
 
-            //add parentheses to encapsulate the mathematical expression because eval wont do it 
+            //add parentheses to encapsulate the mathematical expression because eval won't do it 
             //and produces incorrect results
             for (i = 0; i < expr.split(')').length - 1; i++) {
                 expr = '(' + expr;
@@ -85,6 +101,7 @@ $(document).ready(function () {
 
             $('#working-display').text(result);
             $('#main-display').text(result);
+            wasEquals = true;
         }
     });
 });
